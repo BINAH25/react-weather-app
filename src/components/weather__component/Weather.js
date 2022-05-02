a714c20becbba953a998fe9f9cda53221a774694
BINAH LOUIS <visteen192@gmail.com>
2022-05-02 22:22:54 +0000

    time function and dynamic weather icon added

--- src/components/weather__component/Weather.js
+++ src/components/weather__component/Weather.js
@@ -40,15 +40,28 @@ export default function Weather({ weather }) {
     let month = months[d.getMonth()];
     let year = d.getFullYear();
 
-    return `${day} ${date} ${month} ${year}`;
+    return `${day}, ${date} ${month}, ${year}`;
+  };
+
+  const time = (t) => {
+    const hour = t.getHours();
+    let minute = t.getMinutes();
+    if (minute < 10) {
+      minute = `0${minute}`;
+    } else {
+      minute = minute;
+    }
+
+    return ` ${hour}: ${minute}`;
   };
 
   return (
     <div>
       <div className="container">
-        <div className="row mt-5">
+        <div className="row ">
           <div className="date" data-aos="fade-down-left">
-            {dateBuilder(new Date())}
+            <li>{dateBuilder(new Date())}</li>
+            <li className="time">{time(new Date())}</li>
           </div>
           <div className="col-6 cloud">
             {typeof weather.main != "undefined" ? (
@@ -63,9 +76,17 @@ export default function Weather({ weather }) {
             )}
           </div>
 
-          <div className="col-6 cloud" data-aos="fade-down-right">
-            <img className="pic" src="/icons/perfect-day.svg" alt="" />
-          </div>
+          {typeof weather.main != "undefined" ? (
+            <div className="col-6 cloud" data-aos="fade-down-right">
+              <img
+                className="pic"
+                src={`http://openweathermap.org/img/w/${weather?.weather[0]?.icon}.png`}
+                alt="weather icon"
+              />
+            </div>
+          ) : (
+            ""
+          )}
         </div>
         {typeof weather?.main != "undefined" ? (
           <div className="location" data-aos="fade-down">
@@ -78,19 +99,14 @@ export default function Weather({ weather }) {
         )}
 
         <div className="text">
-          <h5>weather info</h5>
+          <h5>weather Details</h5>
         </div>
         <div className="container ">
           <div className="row ">
             {typeof weather.main != "undefined" ? (
               <div>
                 <div className="col-6 info-container" data-aos="fade-left">
-                  <img
-                    className="icons "
-                    src="/icons/humidity.svg"
-                    alt=""
-                    data-aos="fade-left"
-                  />
+                  <img className="icons " src="/icons/humidity.svg" alt="" />
                   <span className="label" data-aos="fade-left">
                     {weather?.main?.humidity}
                   </span>
